d126ea104ad9fbdc415f06f6a63ac7556dbaca77
Samuel Kristianto <110904319+kaz9112@users.noreply.github.com>
2023-05-12 13:03:01 +0700

    movements done

--- script.js
+++ script.js
@@ -2,22 +2,44 @@
   //use code wasd or arrows to move the point using js function
     // slice the string to have numbers only and then add the numbers movements
     // use conditional or 
-
-
-
 // how to make those leftover lines stays
 // clear all lines
 
-const pointClass = document.querySelector('.point')
-let posTop = getComputedStyle(pointClass).top
-let posLeft = getComputedStyle(pointClass).left
+function pointMovements(e) {
+  const pointClass = document.querySelector('.point')
+  let posTop = getComputedStyle(pointClass).top
+  let posLeft = getComputedStyle(pointClass).left
 
-posTopNum = Number(posTop.slice(0, posTop.length-2))
-posLeftNum = Number(posLeft.slice(0, posLeft.length-2))
+  posTopNum = Number(posTop.slice(0, posTop.length-2))
+  posLeftNum = Number(posLeft.slice(0, posLeft.length-2))
 
-for (let i = 0; i < 10 ; i++) {
-  posLeftNum += 10
-  posLeft = posLeftNum.toString() + 'px'
-  console.log(posLeft)
-  pointClass.style.left = posLeft
+  const key = e.code
+  // console.log(key) // Debug
+
+  if (key == 'ArrowRight') {
+    posLeftNum += 10
+    posLeft = posLeftNum.toString() + 'px'
+    console.log(posLeft)
+    pointClass.style.left = posLeft 
+  } else if (key == 'ArrowLeft') {
+    posLeftNum -= 10
+    posLeft = posLeftNum.toString() + 'px'
+    console.log(posLeft)
+    pointClass.style.left = posLeft 
+  } else if (key == 'ArrowDown') {
+    posTopNum += 10
+    posTop = posTopNum.toString() + 'px'
+    console.log(posTop)
+    pointClass.style.top = posTop 
+  } else if (key == 'ArrowUp') {
+    posTopNum -= 10
+    posTop = posTopNum.toString() + 'px'
+    console.log(posTop)
+    pointClass.style.top = posTop
+  } else {
+    //
+  }
+  
 }
+
+window.addEventListener('keydown', pointMovements);
